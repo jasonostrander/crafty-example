a2a45781f0634eccc7501b38bbf385c999a10c94
Jason Ostrander <jason.ostrander@gmail.com>
2011-03-31 08:34:19 -0700

    Fix white space and remove unneeded var.

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ window.onload = function() {
         {required:17, total:25}
     ]
     var current = 0;
-    var hits = 0;
     var singleton = null;
     var prompt = 'Click to begin';
 
@@ -53,88 +52,87 @@ window.onload = function() {
                         this.w += 1;
                         this.h += 1;
     
-                            if (this._alternate) {
-                                this.x -= 1;
-                                this.y -=1;
-                                this._alternate = false;
-                            } else {
-                                this._alternate = true;
-                            }
-    
-                            if (this.w > 64 ) {
-                                this._state = 'collapsing';
-                            }
-                        } else if (this._state === 'collapsing') {
-                            this.w -= 1;
-                            this.h -= 1;
-    
-                            if (this._alternate) {
-                                this.x += 1;
-                                this.y +=1;
-                                this._alternate = false;
-                            } else {
-                                this._alternate = true;
-                            }
-    
-                            if (this.w < 0 ) {
-                                this._state = 'done';
-                            }
-                        } else if (this._state === 'done') {
-                            this.destroy();
+                        if (this._alternate) {
+                            this.x -= 1;
+                            this.y -=1;
+                            this._alternate = false;
+                        } else {
+                            this._alternate = true;
                         }
-                    });
-                    return this;
-                },
-                restart: function(x,y) {
-                    if (this._state !== 'expanding' && this._state !== 'collapsing') {
-                        // this.w = 16, this.h = 16;
-                        this.x = x, this.y = y;
-                        this._state = 'expanding';
+
+                        if (this.w > 64 ) {
+                            this._state = 'collapsing';
+                        }
+                    } else if (this._state === 'collapsing') {
+                        this.w -= 1;
+                        this.h -= 1;
+
+                        if (this._alternate) {
+                            this.x += 1;
+                            this.y +=1;
+                            this._alternate = false;
+                        } else {
+                            this._alternate = true;
+                        }
+
+                        if (this.w < 0 ) {
+                            this._state = 'done';
+                        }
+                    } else if (this._state === 'done') {
+                        this.destroy();
                     }
-                    return this;
+                });
+                return this;
+            },
+            restart: function(x,y) {
+                if (this._state !== 'expanding' && this._state !== 'collapsing') {
+                    // this.w = 16, this.h = 16;
+                    this.x = x, this.y = y;
+                    this._state = 'expanding';
                 }
-            });
-    
-            // ball component player is trying to hit
-            Crafty.c('ball', {    // "2D, DOM, color, collision, expand")
-                init: function() {
-                    if (!this.has('collision')) this.addComponent('collision');
-                    if (!this.has('expand')) this.addComponent('expand');
-    
-                    this.attr({
-                        x: (Crafty.viewport.width-16) * Math.random(),
-                        y: (Crafty.viewport.height-16)*Math.random(),
-                        w: 16,
-                        h: 16,
-                        xspeed: 1,
-                        yspeed: 2,
-                        expanding: false
-                    })
-                    .bind("enterframe", function() {
-                        this.x += this.xspeed;
-                        this.y += this.yspeed;
-            
-                        if (this._x + this._w > Crafty.viewport.width) {
-                            this.xspeed = -this.xspeed;
-                        } else if (this._x < 0) {
-                            this.xspeed = -this.xspeed;
-                        }
-                        if (this._y + this._h > Crafty.viewport.height) {
-                            this.yspeed = -this.yspeed;
-                        } else if (this._y < 0) {
-                            this.yspeed = -this.yspeed;
-                        }
-                    })
-                    .onhit('expand', function(data) {
-                        if (this.expanding === false && data[0].obj._state === 'expanding') {
-                            this.xspeed = 0, this.yspeed = 0;
-                            this.expand();
-                            this.expanding = true;
-                            score.incrementScore();
-                            hits += 1;
-                        }
-                    });
-                }
+                return this;
+            }
+        });
+
+        // ball component player is trying to hit
+        Crafty.c('ball', {    // "2D, DOM, color, collision, expand")
+            init: function() {
+                if (!this.has('collision')) this.addComponent('collision');
+                if (!this.has('expand')) this.addComponent('expand');
+
+                this.attr({
+                    x: (Crafty.viewport.width-16) * Math.random(),
+                    y: (Crafty.viewport.height-16)*Math.random(),
+                    w: 16,
+                    h: 16,
+                    xspeed: 1,
+                    yspeed: 2,
+                    expanding: false
+                })
+                .bind("enterframe", function() {
+                    this.x += this.xspeed;
+                    this.y += this.yspeed;
+        
+                    if (this._x + this._w > Crafty.viewport.width) {
+                        this.xspeed = -this.xspeed;
+                    } else if (this._x < 0) {
+                        this.xspeed = -this.xspeed;
+                    }
+                    if (this._y + this._h > Crafty.viewport.height) {
+                        this.yspeed = -this.yspeed;
+                    } else if (this._y < 0) {
+                        this.yspeed = -this.yspeed;
+                    }
+                })
+                .onhit('expand', function(data) {
+                    if (this.expanding === false && data[0].obj._state === 'expanding') {
+                        this.xspeed = 0, this.yspeed = 0;
+                        this.expand();
+                        this.expanding = true;
+                        score.incrementScore();
+                    }
+                });
+            }
         });
 
         var score = Crafty.e('2D, DOM, color, text')
